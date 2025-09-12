c174308ec2b98d93095552d717c5baba08fbc107
kVarunkk <varunkumawatleap2@gmail.com>
2025-09-12 12:38:59 +0530

    feat: cache jobs

---
+++ app/jobs/layout.tsx
@@ -0,0 +1,79 @@
+import Error from "@/components/Error";
+import NavbarParent, { INavItem } from "@/components/NavbarParent";
+import { createClient } from "@/lib/supabase/server";
+import { v4 as uuidv4 } from "uuid";
+
+export default async function JobsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  try {
+    const supabase = await createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    const { data: companyData } = await supabase
+      .from("company_info")
+      .select("id, ai_search_uses, filled")
+      .eq("user_id", user?.id)
+      .single();
+
+    let isCompanyUser = false;
+    if (companyData) {
+      isCompanyUser = true;
+    }
+
+    const navItems: INavItem[] = !isCompanyUser
+      ? [
+          {
+            id: uuidv4(),
+            label: "Home",
+            href: "/",
+            type: "equals",
+          },
+          {
+            id: uuidv4(),
+            label: "Find Jobs",
+            href: "/jobs",
+            type: "startswith",
+          },
+        ]
+      : [
+          {
+            id: uuidv4(),
+            label: "Home",
+            href: "/company",
+            type: "equals",
+          },
+          {
+            id: uuidv4(),
+            label: "Job Posts",
+            href: "/company/job-posts",
+            type: "equals",
+          },
+          {
+            id: uuidv4(),
+            label: "Applicants",
+            href: "/company/applicants",
+            type: "equals",
+          },
+          {
+            id: uuidv4(),
+            label: "Profiles",
+            href: "/company/profiles",
+            type: "equals",
+          },
+        ];
+
+    return (
+      <div className="h-screen w-screen">
+        <NavbarParent navItems={navItems} />
+        {children}
+      </div>
+    );
+  } catch {
+    return <Error />;
+  }
+}
